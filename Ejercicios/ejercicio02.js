5ce855f5b52555062e851240645bd476066897f4
pikeran <pikcell1994@gmail.com>
2018-09-25 18:45:19 +0200

    Dia 25 añadiendo ejercicios

--- Ejercicios/ejercicio02.js
+++ Ejercicios/ejercicio02.js
@@ -1,27 +1,39 @@
-var numero = prompt("Ingrese el numero feliz/infeliz");
-
-feliz(numero);
-
-
-
+var contador = 0;
+var felices = 1;
+var numeros = 7;
+var felis = false;
+while (numeros <= 20) {
+    contador = 0;
+    var result = feliz(numeros);
+    document.write(result+"<br>");
+    if (felis) {
+        document.write("<b>" +numeros + " es feliz</b><br>");
+        felices++;
+    } else document.write(numeros + " no es feliz<br>");
+    numeros++;
+    felis = false;
+}
 
 function feliz(numero2) {
-    
-if(numero2==1){
- return;   
-}else{
+    if (numero2 == 1) {
+        document.write("<b>NO entra en contador > 8</b><br>");
+        felis = true;
+        return true;
+    } else {
+        if (contador > 8) {
+            document.write("Entra en contador > 8<br>");
+            return false;
+        }
+        let numerototal = numero2.toString().trim();
+        document.write(numerototal+"<br>CONTADOR:" + contador + "<br>");
+        var suma = 0;
+        for (let i = 0; i < numerototal.length; i++) {
 
-let numerototal = numero2.toString().trim();
-document.write(numerototal[1]);
+            var cuadrado = Number(numerototal[i]) ** 2;
+            suma = suma + cuadrado;
 
-
-document.write("\n cantidad de elementos:"+numerototal.length);
-
-/*
-for (let i = 0; i < arrayfinal.length; i++) {
-    
-    document.write("\n"+arrayfinal[i]);
-
-}*/
-}
+        }
+        contador++;
+        feliz(suma);
+    }
 }
