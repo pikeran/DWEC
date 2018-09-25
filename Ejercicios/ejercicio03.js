938a9a812ff6d3d123eaa10a3048b22e162326e2
pikeran <pikcell1994@gmail.com>
2018-09-25 18:48:23 +0200

    Corrección de errores.

--- Ejercicios/ejercicio03.js
+++ Ejercicios/ejercicio03.js
@@ -36,7 +36,7 @@ function PedirDni() {
 
 function PedirCorreo() {
     
-    
+    var correo = prompt("Introduzca su correo: ");
     var expresion_regular_correo = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
     if(expresion_regular_correo.test(correo)){
