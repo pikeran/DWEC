c443c0d97ea00d612a4190eb77c723084905e5f5
pikeran <pikcell1994@gmail.com>
2018-11-07 16:03:50 +0100

    Actualizacion de repositorios

--- Ejercicios/ej_12/ejercicio12.js
+++ Ejercicios/ej_12/ejercicio12.js
@@ -8,15 +8,53 @@ function cargar(){
     document.getElementById("pausa").addEventListener("click",pauseVideo,false);
     document.getElementById("adelante").addEventListener("click",adelanteVideo,false);
     document.getElementById("vol-up").addEventListener("click",subirVolumen,false);
-    document.getElementById("vol-down").addEventListener("click",bajarVolumen,false); 
+    document.getElementById("vol-down").addEventListener("click",bajarVolumen,false);
+    document.getElementById("video-01").addEventListener("timeupdate",actualizarBarra,false);
+    document.getElementById("contenedor-1").addEventListener("mouseover",difuminar,false);
+    document.getElementById("contenedor-2").addEventListener("mouseover",difuminar,false);
+    document.getElementById("contenedor-3").addEventListener("mouseover",difuminar,false); 
+    document.getElementById("contenedor-1").addEventListener("mouseout",offDifuminar,false);
+    document.getElementById("contenedor-2").addEventListener("mouseout",offDifuminar,false);
+    document.getElementById("contenedor-3").addEventListener("mouseout",offDifuminar,false); 
+    document.getElementById("contenedor-1").addEventListener("click",cambiarVideo,false);
+    document.getElementById("contenedor-2").addEventListener("click",cambiarVideo,false);
+    document.getElementById("contenedor-3").addEventListener("click",cambiarVideo,false);
 }
+var videoReproductor = "video-01";
+
+
 function obtenerVideo() {
-    var multimedia = document.getElementById("video-01"); 
+    var multimedia = document.getElementById(videoReproductor); 
     return multimedia;
+}
+function cambiarVideo(){
+    elemento = this.id;
+    multimedia = obtenerVideo();
+    principalSrc = multimedia.src;
+    alert(principalSrc);
+}
+function offDifuminar(){
+    document.body.style.cursor = "auto";
+    this.style.opacity=100;
+}
+function difuminar(){ 
+    document.body.style.cursor = "Pointer";
+    this.style.opacity=(50/100)-0.01;
+}
+//A mano compañero:
+function actualizarBarra(){
+    multimedia = obtenerVideo();
+    tiempo = getTime();
+    var duracion = multimedia.duration;
+    var media1 = tiempo/duracion;
+    var resultado = media1*100;
+
+    document.getElementById("progreso").style.width = resultado+"%";
 } 
 function getTime(){
     multimedia = obtenerVideo();
-    alert(multimedia.currentTime);
+    var tiempo = multimedia.currentTime;
+    return tiempo;
 }
 function playVideo(){
     multimedia = obtenerVideo();
@@ -36,11 +74,11 @@ function adelanteVideo(){
 }
 function subirVolumen(){
     multimedia = obtenerVideo();
-    multimedia.volume = multimedia.volume + 0.5; 
+    multimedia.volume = multimedia.volume + 0.2; 
 }
 function bajarVolumen(){
     multimedia = obtenerVideo();
-    multimedia.volume = multimedia.volume - 0.5;   
+    multimedia.volume = multimedia.volume - 0.2;   
 }
 
 
